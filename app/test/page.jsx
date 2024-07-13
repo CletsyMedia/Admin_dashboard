a7993d1c2da6b5c5025e13af4b0f22d1fe46135b
CletsyMedia <Cletussam12@yahoo.com>
2024-07-13 23:05:09 +0100

    Performing server actions

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,2 +1,12 @@
-// Server actions 
-rafce
+"use client"
+import { useState } from "react"
+
+// Server actions
+const Page = () => {
+  const [input, setInput] = useState()
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page
