d8fc46231a0f54c34d5e0acb011054134a8d9e01
CletsyMedia <Cletussam12@yahoo.com>
2024-07-13 12:54:23 +0100

    Fetching products from DB

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,61 +1,65 @@
 import Link from "next/link";
-import { format } from "date-fns";
 import { FiUserPlus, FiEye, FiTrash } from "react-icons/fi";
 import Pagination from "../../ui/dashboard/pagination/pagination";
 import Search from "../../ui/dashboard/search/search";
+import { fetchingProducts } from "../../lib/data";
+import {searchParams} from "next/navigation"
 
-const ProductData = [
-  {
-    id: 1,
-    title: "Premium Leather Wallet",
-    description: "Handcrafted premium leather wallet with RFID protection.",
-    price: 79.99,
-    created: new Date(2024, 6, 24),
-    stock: 50,
-    image: "/wallet.jpg"
-  },
-  {
-    id: 2,
-    title: "Vintage Wooden Desk Organizer",
-    description: "Elegant wooden desk organizer with multiple compartments.",
-    price: 49.99,
-    created: new Date(2023, 10, 15),
-    stock: 25,
-    image: "/desk.jpg"
-  },
-  {
-    id: 3,
-    title: "Modern Ceramic Plant Pots",
-    description: "Set of 3 modern ceramic plant pots for indoor and outdoor use.",
-    price: 29.99,
-    created: new Date(2022, 3, 5),
-    stock: 100,
-    image: "/ceramic.jpg"
-  },
-  {
-    id: 4,
-    title: "Stylish Stainless Steel Watch",
-    description: "Premium stainless steel watch with automatic movement.",
-    price: 199.99,
-    created: new Date(2023, 7, 12),
-    stock: 10,
-    image: "/stainless_watch.jpg"
-  },
-  {
-    id: 5,
-    title: "Luxury Leather Briefcase",
-    description: "Luxury leather briefcase with multiple compartments and laptop sleeve.",
-    price: 299.99,
-    created: new Date(2024, 1, 28),
-    stock: 15,
-    image: "https://source.unsplash.com/500x500/?briefcase,product"
-  },
-];
+// const ProductData = [
+//   {
+//     id: 1,
+//     title: "Premium Leather Wallet",
+//     description: "Handcrafted premium leather wallet with RFID protection.",
+//     price: 79.99,
+//     created: new Date(2024, 6, 24),
+//     stock: 50,
+//     image: "/wallet.jpg"
+//   },
+//   {
+//     id: 2,
+//     title: "Vintage Wooden Desk Organizer",
+//     description: "Elegant wooden desk organizer with multiple compartments.",
+//     price: 49.99,
+//     created: new Date(2023, 10, 15),
+//     stock: 25,
+//     image: "/desk.jpg"
+//   },
+//   {
+//     id: 3,
+//     title: "Modern Ceramic Plant Pots",
+//     description: "Set of 3 modern ceramic plant pots for indoor and outdoor use.",
+//     price: 29.99,
+//     created: new Date(2022, 3, 5),
+//     stock: 100,
+//     image: "/ceramic.jpg"
+//   },
+//   {
+//     id: 4,
+//     title: "Stylish Stainless Steel Watch",
+//     description: "Premium stainless steel watch with automatic movement.",
+//     price: 199.99,
+//     created: new Date(2023, 7, 12),
+//     stock: 10,
+//     image: "/stainless_watch.jpg"
+//   },
+//   {
+//     id: 5,
+//     title: "Luxury Leather Briefcase",
+//     description: "Luxury leather briefcase with multiple compartments and laptop sleeve.",
+//     price: 299.99,
+//     created: new Date(2024, 1, 28),
+//     stock: 15,
+//     image: "https://source.unsplash.com/500x500/?briefcase,product"
+//   },
+// ];
 
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
+  const q = searchParams?.q || "";
+  // Pagination if no page 1 by default
+  const page = searchParams?.page || 1;
+  const { count, products } = await fetchingProducts(q, page);
 
-  const formatDate = (date) => format(date, "MMM d, yyyy");
 
   const trimDescription = (description, maxLength) => {
     if (description.length > maxLength) {
@@ -97,7 +101,7 @@ const ProductsPage = () => {
           </tr>
         </thead>
         <tbody>
-          {ProductData.map((product) => (
+          {products.map((product) => (
             <tr key={product.id}>
               <td>
                 <label htmlFor={`checkbox${product.id}`}>
@@ -112,7 +116,7 @@ const ProductsPage = () => {
                 <div className="flex items-center gap-3">
                   <div className="avatar">
                     <div className="mask mask-circle h-12 w-12">
-                      <img src={product.image} alt="Product" className="rounded-full" />
+                      <img src={product.img || "/noproduct.jpg "} alt="Product" className="rounded-full" />
                     </div>
                   </div>
                   <div>
@@ -122,18 +126,18 @@ const ProductsPage = () => {
               </td>
               <td className="relative">
               <div className="cursor-pointer">
-                  {trimDescription(product.description, 30)}
+                  {trimDescription(product.desc, 30)}
                 </div>
                 {/* <div className="absolute insert-0 top-0 left-0 right-0 bg-slate-600 p-4 rounded-md">
                   {product.description}
                 </div> */}
               </td>
               <td>${product.price.toFixed(2)}</td>
-              <td>{formatDate(product.created)}</td>
+              <td>{product.createdAt.toString().slice(4,16)}</td>
               <td>{product.stock}</td>
               <td>
                 <div className="flex gap-2">
-                  <Link href="/dashboard/products/id">
+                  <Link href={`/dashboard/products/${product.id}`}>
                     <button
                       className="flex items-center justify-center px-3 py-1 text-blue-500 border border-blue-500 rounded-md hover:bg-blue-500 hover:text-white"
                       title="View"
@@ -157,7 +161,7 @@ const ProductsPage = () => {
           ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
     </div>
    );
--- app/lib/data.jsx
+++ app/lib/data.jsx
@@ -1,4 +1,5 @@
 import { User } from "./models";
+import { Product } from "./models";
 import { connect2DB } from "./utils";
 
 export const fetchingUsers = async (q, page) => {
@@ -13,6 +14,25 @@ export const fetchingUsers = async (q, page) => {
       .skip(itemsPerPage * (page - 1))
       .limit(itemsPerPage);
     return {count, users}
+    
+  } catch (error) {
+    console.error("Error fetching user data", error);
+    throw new Error("Failed to fetch user data")
+  }
+};
+
+export const fetchingProducts = async (q, page) => {
+  const regex = new RegExp(q, "i")
+  // Pagination
+  const itemsPerPage = 2;
+
+  try {
+    connect2DB();
+    const count = await Product.find({ title: { $regex: regex } }).countDocuments();
+    const products = await Product.find({ title: { $regex: regex } })
+      .skip(itemsPerPage * (page - 1))
+      .limit(itemsPerPage);
+    return {count, products}
     
   } catch (error) {
     console.error("Error fetching user data", error);
